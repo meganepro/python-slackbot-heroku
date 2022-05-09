b4ba384184449460996bfedd3cad43e6717d2b3e
isamu tajima <136.tajima@gmail.com>
2022-05-09 22:19:58 +0900

    add slash command

--- custom/command.py
+++ custom/command.py
@@ -5,5 +5,11 @@ def listners(app: App):
 
     # command(): ()内のアクションに反応する
     @app.command("/hello-bolt")
-    def hello(body, ack):
-        ack(f"Hi <@{body['user_id']}>!")
+    def hello(ack, say, command):
+        ack()
+        user = f"<@{command.get('user_id','ななしさん')}>"
+        text = command.get('text', "")
+        channel = f"<#{command.get('channel_id','知らないチャンネル')}>"
+
+        ################## write code here ##################
+        say(f'Hi! {user}! at {channel}.\nYou say {text}')
--- index.py
+++ index.py
@@ -1,52 +1,45 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 import os
-import json
-from slack_bolt import App as SlackBolt
+from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
-from flask import Flask, request, Response
+from flask import Flask, request
 
 
 # boltの初期化
-bolt = SlackBolt(
+app = App(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
     token=os.environ.get("SLACK_BOT_TOKEN")
 )
-handle = SlackRequestHandler(bolt)
 
 
 # Flask アプリを初期化
-app = Flask(__name__)
+flask_app = Flask(__name__)
+handle = SlackRequestHandler(app)
 
 
 # Flask アプリへのルートを登録します
-@app.route("/slack/events", methods=["POST"])
+@flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
-    data = request.data.decode('utf-8')
-    data = json.loads(data)
-    if 'challenge' in data:
-        token = str(data['challenge'])
-        return Response(token, mimetype='text/plane')
-    # handle はアプリのディスパッチメソッドを実行します
     return handle.handle(request)
 
 
 ##########################################################################
 # custom actionの設定
 import custom.message
-custom.message.listners(bolt)
+custom.message.listners(app)
 import custom.event_mention
-custom.event_mention.listners(bolt)
+custom.event_mention.listners(app)
 import custom.event_message
-custom.event_message.listners(bolt)
+custom.event_message.listners(app)
 import custom.event_stamp
-custom.event_stamp.listners(bolt)
+custom.event_stamp.listners(app)
 import custom.command
-custom.command.listners(bolt)
+custom.command.listners(app)
 ##########################################################################
 
 
 # ローカルでのアプリの起動設定
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    flask_app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
