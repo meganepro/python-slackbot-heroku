2661dde0c56687121e5a8d89a888c15de74f4bf8
isamu tajima <136.tajima@gmail.com>
2022-05-09 22:37:24 +0900

    rename

--- index.py
+++ index.py
@@ -1,25 +1,25 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 import os
-from slack_bolt import App
+from slack_bolt import App as SlackBolt
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, request
 
 
 # boltの初期化
-app = App(
+bolt = SlackBolt(
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
     token=os.environ.get("SLACK_BOT_TOKEN")
 )
 
 
 # Flask アプリを初期化
-flask_app = Flask(__name__)
-handle = SlackRequestHandler(app)
+app = Flask(__name__)
+handle = SlackRequestHandler(bolt)
 
 
 # Flask アプリへのルートを登録します
-@flask_app.route("/slack/events", methods=["POST"])
+@app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handle.handle(request)
 
@@ -27,19 +27,19 @@ def slack_events():
 ##########################################################################
 # custom actionの設定
 import custom.message
-custom.message.listners(app)
+custom.message.listners(bolt)
 import custom.event_mention
-custom.event_mention.listners(app)
+custom.event_mention.listners(bolt)
 import custom.event_message
-custom.event_message.listners(app)
+custom.event_message.listners(bolt)
 import custom.event_stamp
-custom.event_stamp.listners(app)
+custom.event_stamp.listners(bolt)
 import custom.command
-custom.command.listners(app)
+custom.command.listners(bolt)
 ##########################################################################
 
 
 # ローカルでのアプリの起動設定
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
-    flask_app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
