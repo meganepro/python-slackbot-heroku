9255ce0bf1f79cf351b8fe55dc01f0b3dbe6245b
isamu tajima <136.tajima@gmail.com>
2022-05-11 22:44:49 +0900

    add call api sample

--- custom/event_mention.py
+++ custom/event_mention.py
@@ -1,5 +1,5 @@
 from slack_bolt import App
-from custom.util import is_include
+from custom.util import is_include, extract
 
 
 def listners(app: App):
@@ -11,25 +11,58 @@ def listners(app: App):
         channel = f"<#{event.get('channel','知らないチャンネル')}>"
 
         ################## write code here ##################
+        # おみくじ
         if(is_include(text, "おみくじ")):
             kekka = uranai()
             say(user + " さんは " + kekka + "です！")
+        # 郵便番号から住所
+        elif(is_include(text, "の住所は？")):
+            zipcode = extract(text, "", "の住所は")
+            if zipcode:
+                place = zipcode2address(zipcode)
+                # 住所が取得できた場合
+                if place:
+                    say(place + "です！")
+                else:
+                    say("住所がうまく検索できませんでした！")
+            else:
+                say("郵便番号がよくわかりませんでした！")
         else:
             say(user + "さん、よびました？？")
 
-    # uranai結果を返す
-    def uranai():
-        import random
-        number = random.randrange(1, 11)
-        uranai = "大凶 :skull_and_crossbones:"
-        if number <= 1:
-            uranai = "大吉 :tada:"
-        elif number <= 3:
-            uranai = "中吉 :mega:"
-        elif number <= 7:
-            uranai = "吉 :neutral_face:"
-        elif number <= 9:
-            uranai = "凶 :ghost:"
-        print("おみくじの結果→→→→→→→→→→" + uranai)
 
-        return uranai
+# uranai結果を返す
+def uranai():
+    import random
+    number = random.randrange(1, 11)
+    uranai = "大凶 :skull_and_crossbones:"
+    if number <= 1:
+        uranai = "大吉 :tada:"
+    elif number <= 3:
+        uranai = "中吉 :mega:"
+    elif number <= 7:
+        uranai = "吉 :neutral_face:"
+    elif number <= 9:
+        uranai = "凶 :ghost:"
+    print("おみくじの結果→→→→→→→→→→" + uranai)
+
+    return uranai
+
+
+# zipcodeを住所に変換する
+def zipcode2address(zipcode: str):
+    import json
+    import requests
+    response = requests.get("https://zipcloud.ibsnet.co.jp/api/search", params={"zipcode": zipcode})
+    results = json.loads(response.text)
+    # 住所が取れなかった場合はNone（なにもない）を返す
+    if (results['results'] is None):
+        return None
+    # 住所が取得できた場合の処理
+    result = results['results'][0]
+    address = result['address1'] + result['address2'] + result['address3']
+    return address
+
+
+if __name__ == "__main__":
+    print(zipcode2address("15000011"))
--- custom/util.py
+++ custom/util.py
@@ -12,3 +12,23 @@ def is_include(text: str, target: str):
         _type_: _description_
     """
     return True if len(re.findall(target, text)) > 0 else False
+
+
+def extract(text: str, before: str, after: str):
+    """before と afterに挟まれた単語を抽出する
+
+    Args:
+        text (str): _description_
+        before (str): _description_
+        after (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    regex = f'{before}(\w+){after}'
+    matches = re.findall(regex, text)
+    return matches[0] if len(matches) > 0 else ""
+
+
+if __name__ == '__main__':
+    print(extract("hogefugahogemiga", "fuga", 'miga'))
