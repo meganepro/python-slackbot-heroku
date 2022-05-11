7eac1d30af5591fc032a38277c3b24197e73ba79
isamu tajima <136.tajima@gmail.com>
2022-05-11 23:05:40 +0900

    add send image sample

--- custom/message.py
+++ custom/message.py
@@ -12,3 +12,29 @@ def listners(app: App):
 
         ################## write code here ##################
         say("Hey there " + user + ", at " + channel + "!")
+
+    @app.message("いめーじさんぷる")
+    def reploy_image_sample(message, say):
+        user = f"<@{message.get('user','ななしさん')}>"
+        text = message.get('text', "")
+        channel = f"<#{message.get('channel','知らないチャンネル')}>"
+
+        ################## write code here ##################
+        # https://app.slack.com/block-kit-builder/T03CPLVRW4S#%7B%22blocks%22:%5B%7B%22type%22:%22section%22,%22text%22:%7B%22type%22:%22plain_text%22,%22text%22:%22This%20is%20a%20plain%20text%20section%20block.%22,%22emoji%22:true%7D%7D,%7B%22type%22:%22image%22,%22image_url%22:%22https://i1.wp.com/thetempest.co/wp-content/uploads/2017/08/The-wise-words-of-Michael-Scott-Imgur-2.jpg?w=1024&ssl=1%22,%22alt_text%22:%22inspiration%22%7D%5D%7D
+        say({
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "This is a plain text section block.",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "image",
+                    "image_url": "https://i1.wp.com/thetempest.co/wp-content/uploads/2017/08/The-wise-words-of-Michael-Scott-Imgur-2.jpg?w=1024&ssl=1",
+                    "alt_text": "inspiration"
+                }
+            ]
+        })
